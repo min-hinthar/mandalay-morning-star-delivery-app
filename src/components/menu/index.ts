18c68921a509b50bff9c8df6c6e2781c919530c1
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2026-01-14 14:55:17 -0800

    feat: implement item card v1

--- src/components/menu/index.ts
+++ src/components/menu/index.ts
@@ -3,3 +3,5 @@ export { CategoryTabs } from "./category-tabs";
 export { MenuSection } from "./menu-section";
 export { MenuItemCard } from "./menu-item-card";
 export { MenuSkeleton } from "./menu-skeleton";
+export { ItemCard } from "./ItemCard";
+export { ItemCardSkeleton } from "./ItemCardSkeleton";
